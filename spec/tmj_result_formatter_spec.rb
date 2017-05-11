47a242a2711527cb97f8f093afaf44c7a8fb7063
Fedor Barannik <fedor.barannik@loblaw.ca>
2017-05-11 14:49:15 -0400

    Update result formatter

--- spec/tmj_result_formatter_spec.rb
+++ spec/tmj_result_formatter_spec.rb
@@ -1,9 +1,13 @@
 require "spec_helper"
 
-RSpec.describe TMJResultFormatter do
-  it "example" do |e|
+RSpec.describe 'TMJResultFormatter' do
+  it "example", test_id: 'CC-T913s' do |e|
+    pending('message')
     e.step 'check version' do
       expect(TMJFormatter::VERSION).not_to be nil
+    end
+
+    e.step 'test' do
     end
   end
 end

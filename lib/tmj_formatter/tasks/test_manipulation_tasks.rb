92ada65842106a9df4b19c998bc94d5184b06939
Fedor Barannik <frbk@users.noreply.github.com>
2017-05-18 17:46:50 -0400

    Update test_manipulation_tasks.rb

--- lib/tmj_formatter/tasks/test_manipulation_tasks.rb
+++ lib/tmj_formatter/tasks/test_manipulation_tasks.rb
@@ -13,7 +13,7 @@ module TMJ
       namespace 'tmj' do
         desc 'Create test cases on Test Managment For JIRA'
         task :create, [:path] do |t, args|
-          exec("bundle exec rspec #{args[:path] if args[:path]} --format TMJCreateTestFormatter --dry-run -r ./lib/tmj_formatter/example")
+          exec("bundle exec rspec #{args[:path] if args[:path]} --format TMJCreateTestFormatter --dry-run -r tmj_formatter/example")
         end
 
         desc 'Update test cases on Test Managment For JIRA'

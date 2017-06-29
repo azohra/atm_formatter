af476875d2fbb17f96317079349b1f57f738741b
Fedor Barannik <frbk@users.noreply.github.com>
2017-06-29 14:53:57 -0400

    Resolve #15.  Add the ability to set custom labels from specs.

--- lib/tmj_formatter/helpers/base_formatter.rb
+++ lib/tmj_formatter/helpers/base_formatter.rb
@@ -1,45 +1,49 @@
 require 'rspec/core/formatters/base_formatter'
 module TMJFormatter
-class BaseFormatter < RSpec::Core::Formatters::BaseFormatter
-  DEFAULT_OPTIONS = { update_existing_tests: false, test_owner: nil, custom_labels: nil}.freeze
+  class BaseFormatter < RSpec::Core::Formatters::BaseFormatter
+    DEFAULT_OPTIONS = { update_existing_tests: false, test_owner: nil, custom_labels: [] }.freeze
 
-  NOTIFICATIONS = %i[start, example_started].freeze
+    NOTIFICATIONS = %i[start example_started].freeze
 
-  def start(_notification)
-    @options = DEFAULT_OPTIONS.merge(TMJFormatter.config.create_test_formatter_options)
-    @client = TMJ::Client.new(TMJFormatter.config.to_hash)
+    def start(_notification)
+      @options = DEFAULT_OPTIONS.merge(TMJFormatter.config.create_test_formatter_options)
+      @client = TMJ::Client.new(TMJFormatter.config.to_hash)
+    end
+
+    private
+
+    def process_example(example)
+      {
+          "projectKey":   configure_project_key,
+          "name":         example.metadata[:full_description],
+          "objective":    example.metadata[:objective],
+          "precondition": example.metadata[:precondition],
+          "folder":       example.metadata[:folder],
+          "status":       example.metadata[:status],
+          "priority":     example.metadata[:priority],
+          "owner":        @options[:test_owner],
+          "labels":       configure_labels(example),
+          "testScript":   process_steps(example.metadata[:steps])
+      }.delete_if { |k, v| v.nil? || v.empty?}
+    end
+
+    def configure_project_key
+      self.class.to_s == 'TMJUpdateTestFormatter' ? nil : TMJFormatter.config.project_id
   end
 
-  private
+    def configure_labels(example)
+      return @options[:custom_labels] if example.metadata[:label].nil?
+      @options[:custom_labels].push(example.metadata[:label]).flatten!
+    end
 
-  def process_example(example)
-    {
-        "projectKey":   configure_project_key,
-        "name":         example.metadata[:full_description],
-        "objective":    example.metadata[:objective],
-        "precondition": example.metadata[:precondition],
-        "folder":       example.metadata[:folder],
-        "status":       example.metadata[:status],
-        "priority":     example.metadata[:priority],
-        "owner":        @options[:test_owner],
-        "labels":       @options[:custom_labels],
-        "testScript":   process_steps(example.metadata[:steps])
-    }.delete_if { |k, v| v.nil? || v.empty?}
+    def process_steps(example)
+      return unless example
+      arr = []
+      example.each { |s| arr << {"description": s[:step_name]} }
+      {
+          "type": "STEP_BY_STEP",
+          "steps": arr
+      }
+    end
   end
-
-
-  def configure_project_key
-    self.class.to_s == 'TMJUpdateTestFormatter' ? nil : TMJFormatter.config.project_id
-  end
-
-  def process_steps(example)
-    return unless example
-    arr = []
-    example.each { |s| arr << {"description": s[:step_name]} }
-    {
-        "type": "STEP_BY_STEP",
-        "steps": arr
-    }
-  end
-end
 end
